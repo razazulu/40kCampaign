0259fa53a66209afc5171c2004ad742627888772
razazulu <razazulu@gmail.com>
2016-02-22 14:30:33 +0000

    First

---
+++ 40kCampaign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_40kCampaign.Startup))]
+namespace _40kCampaign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
